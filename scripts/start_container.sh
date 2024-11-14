f4403286352b1760f8abeececc4251dd66aa1eb6
iam-rayees <168510920+iam-rayees@users.noreply.github.com>
2024-11-15 01:39:55 +0530

    port changed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull rayeez/simple-flask-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 rayeez/simple-flask-python-app:latest 
+docker run -d -p 5000:5000 rayeez/simple-flask-python-app:latest 
 
