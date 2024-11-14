541ad00c70238b7e3c54f647db96ebb0fc5cb122
iam-rayees <168510920+iam-rayees@users.noreply.github.com>
2024-11-15 01:14:42 +0530

    add the files for codedeploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull rayeez/simple-flask-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 8000:8000 rayeez/simple-flask-python-app:latest
+
