ebb7a6ce8685ddd386de461a29432d66b5a4ab42
iam-rayees <168510920+iam-rayees@users.noreply.github.com>
2024-11-15 02:31:46 +0530

    added rmi command in script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,4 +4,6 @@ set -e
 # Stop the running container (if any)
 containerid=`docker ps | awk -F " " '{print $1}'`
 docker rm -f $containerid 
-  
+
+imageid=`docker images | awk -F '{print $3}'`
+docker rmi -f $imageid
