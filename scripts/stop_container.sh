7969c7e20217844ef2315e283aa800d3823c329d
iam-rayees <168510920+iam-rayees@users.noreply.github.com>
2024-11-15 20:03:28 +0530

    first time-No stop script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`docker ps | awk -F " " '{print $1}'`
-docker rm -f $containerid 
+echo "Hi"
+#containerid=`docker ps | awk -F " " '{print $1}'`
+#docker rm -f $containerid 
 
-imageid=`docker images | awk -F " " '{print $3}'`
-docker rmi -f $imageid
+#imageid=`docker images | awk -F " " '{print $3}'`
+#docker rmi -f $imageid
